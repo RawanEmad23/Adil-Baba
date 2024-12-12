a2087444b97c8cf393a220e19bc6bded25561c01
mo7amed4a <m7md0a99@gmail.com>
2024-12-12 11:15:25 +0200

    edit chat

--- src/app/(dashboard)/dashboard/communication/chat/page.tsx
+++ src/app/(dashboard)/dashboard/communication/chat/page.tsx
@@ -1,7 +1,6 @@
+import { redirect } from 'next/navigation'
 import React from 'react'
 
 export default function Page() {
-  return (
-    <div>page</div>
-  )
+  redirect('/dashboard/communication/chat/1')
 }
