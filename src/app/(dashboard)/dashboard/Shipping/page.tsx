d1e26497a0696aecbc2b34d0d3a321362ca0b2d9
RawanEmad23 <153556686+RawanEmad23@users.noreply.github.com>
2025-02-07 18:13:26 +0200

    Update page.tsx

--- src/app/(dashboard)/dashboard/Shipping/page.tsx
+++ src/app/(dashboard)/dashboard/Shipping/page.tsx
@@ -35,7 +35,7 @@ export default function ProductsInventory() {
   };
 
   return (
-    <div className="p-4 bg-gray-100 min-h-screen">
+    <div className="p-4 min-h-screen">
       <div className="container mx-auto">
       <div className="flex justify-between items-center mb-8 flex-col sm:flex-row">
   <h1 className="text-2xl font-semibold">Shipping</h1>
